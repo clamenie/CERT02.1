63a2104a7ed9e3499604ff8d9172dbb030991cd4
erwannFace <e.duclos@fondationface.org>
2018-01-03 23:06:23 +0100

    Preparing login form

--- login.php
+++ login.php
@@ -1,9 +1,21 @@
-<!DOCTYPE html>
+<?php
+	session_start();
+	include 'src/dao/DBController.php';
+?>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
 
+<?php
+		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	<form class="" action="#" method="post">
+		<label for="email">E-mail :</label>
+		<input type="email" name="email" value="">
+		<label for="password">Password :</label>
+		<input type="password" name="password" value="">
+		<button type="submit" name="button">Login</button>
+	</form>
 </body>
 </html>
